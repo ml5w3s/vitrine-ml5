39f382423d83c596b02c15c9a85d971e08206a67
ml5w3s <marcelo.lauriano@gmail.com>
2025-10-17 11:06:42 -0300

    chore: Add remaining untracked files and stage deletions

--- mlab5/src/assets/js/ui/components/temp/navbar.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const menuToggle = document.getElementById('menu-toggle');
-  const navList = document.getElementById('nav-list');
-
-  menuToggle.addEventListener('click', () => {
-    navList.classList.toggle('active');
-  });
-});
